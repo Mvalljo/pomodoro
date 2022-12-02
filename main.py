022decdd8146f2b8bcdbedf97ab270232034e738
Snorlax <maria.vallejo29@yahoo.com>
2022-12-02 14:40:40 -0600

    finished UI setup

--- main.py
+++ main.py
@@ -23,8 +23,20 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 canvas = Canvas(width=200, height=223, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
+canvas.create_image(100, 110, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+title_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+start_btn = Button(text="Start", highlightthickness=0)
+start_btn.grid(column=0, row=2)
+
+reset_btn = Button(text="Reset", highlightthickness=0)
+reset_btn.grid(column=2, row=2)
+
+check_label = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
 
 window.mainloop()
