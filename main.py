956cd4fb1e41a4accd103dd83634b5649e533d60
Snorlax <maria.vallejo29@yahoo.com>
2022-12-05 15:00:35 -0600

    update time for break and work

--- main.py
+++ main.py
@@ -28,12 +28,9 @@ def start_timer():
     global reps
     reps += 1
 
-    # work_sec = WORK_MIN * 60
-    # short_break_sec = SHORT_BREAK_MIN * 60
-    # long_break_sec = LONG_BREAK_MIN * 60
-    work_sec = 2
-    short_break_sec = 2
-    long_break_sec = 2
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
     if reps % 8 == 0:
         count_down(long_break_sec)
